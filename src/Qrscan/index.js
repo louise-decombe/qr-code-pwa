39235b3771b1f8cea13989887cc4c8fee3d3e90b
louise-decombe <louise.decombe@laplateforme.io>
2023-05-07 12:22:39 +0200

    update react 17 version / add module qrscan component

---
+++ src/Qrscan/index.js
@@ -0,0 +1,40 @@
+
+import React, { useState } from 'react';
+import { QrReader } from 'react-qr-reader';
+
+
+import styles from './Qrscan.module.css';
+
+const Qrscan = () => {
+
+	const [result, setResult] = useState('Pas de résultat');
+
+	const handleError = (err) => {
+		console.err(err)
+	}
+
+	const handleScan = (result) => {
+		if(result){
+			setResult(result)
+		}
+	}
+
+	const previewStyle = {
+		height: 240,
+		width: 320,
+	}
+
+	return (
+		<div className={styles.container}>
+			<QrReader
+			delay={500}
+			style={previewStyle}
+			onError={handleError}
+			onScan={handleScan}
+			/>
+			<div className={styles.result}>{result}</div>		
+		</div>
+	);
+}
+
+export default Qrscan;
