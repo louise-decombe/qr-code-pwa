c05a06137399aabd0007ae21783ab20bb92fe166
louise-decombe <louise.decombe@laplateforme.io>
2023-05-08 08:56:33 +0200

    update qrscan function refacto

--- src/Qrscan/index.js
+++ src/Qrscan/index.js
@@ -1,29 +1,27 @@
 import React, { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
 
-const Qrscan = () => {
-  const [result, setResult] = useState('No result');
-
-  const handleScan = (data) => {
-    if (data) {
-      setResult(data);
-    }
-  };
-
-  const handleError = (err) => {
-    console.error(err);
-  };
+const Qrscan = (props) => {
+  const [data, setData] = useState('No result');
 
   return (
-    <div>
+    <>
       <QrReader
-        delay={300}
-        onError={handleError}
-        onResult={handleScan} 
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+            window.open(result.text, '_blank'); // Open the link in a new tab
+
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
         style={{ width: '100%' }}
       />
-      <p>{result}</p>
-    </div>
+      <p>{data}</p>
+    </>
   );
 };
 
